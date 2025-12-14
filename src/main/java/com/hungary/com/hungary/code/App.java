1e69ab49d9349df3f4e597dd50c6feceeebc4fca
Gauti <gautam12994@gmail.com>
2025-12-14 18:40:12 +0530

    Committing changes

---
+++ src/main/java/com/hungary/com/hungary/code/App.java
@@ -0,0 +1,13 @@
+package com.hungary.com.hungary.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
